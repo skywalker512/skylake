6d146a13a3a2a971f7be3c5d2112c46ae271a570
skywalker512 <houzhenhong@outlook.com>
2022-05-23 12:20:05 +0800

    chore: use turborepo

--- apps/skylake/vite.config.ts
+++ apps/skylake/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer'
+import inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
+  plugins: [
+    react(),
+    visualizer({ open: true }),
+    process.env.INSPECT && inspect(),
+  ],
 })
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -5,4 +5,5 @@ export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
   format: ['esm'],
+  sourcemap: true,
 })
