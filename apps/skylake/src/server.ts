f5b7bba36bb1f5253b6b54cbee25a2c87b7b9a18
skywalker512 <houzhenhong@outlook.com>
2022-05-29 10:32:49 +0800

    chore: update

--- apps/skylake/src/server.ts
+++ apps/skylake/src/server.ts
@@ -3,6 +3,14 @@ import { Server } from '@hocuspocus/server'
 import { slateNodesToInsertDelta } from '@slate-yjs/core'
 import { Descendant } from 'slate'
 import * as Y from 'yjs'
+import { SQLite } from '@hocuspocus/extension-sqlite'
+import path from 'path'
+// import { createRequire } from 'module'
+
+// const require = createRequire(import.meta.url)
+
+// const { Monitor } =
+//   require('@hocuspocus/extension-monitor') as typeof import('@hocuspocus/extension-monitor')
 
 const initialValue: Descendant[] = [{ children: [{ text: '' }] }]
 
@@ -11,7 +19,10 @@ const server = Server.configure({
   port: 1234,
 
   // Add logging
-  extensions: [new Logger()],
+  extensions: [
+    new Logger(),
+    new SQLite({ database: path.join(__dirname, 'data.db') }),
+  ],
 
   async onLoadDocument(data) {
     // Load the initial value in case the document is empty
